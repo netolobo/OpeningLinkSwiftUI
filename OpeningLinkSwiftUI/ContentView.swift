47c1f72439a17161d0038279240a9c6df48a65e1
netolobo <desilio@gmail.com>
2024-01-18 14:47:04 -0800

    Initial Commit

---
+++ OpeningLinkSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpeningLinkSwiftUI
+//
+//  Created by Neto Lobo on 18/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpeningLinkSwiftUI/OpeningLinkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningLinkSwiftUIApp.swift
+//  OpeningLinkSwiftUI
+//
+//  Created by Neto Lobo on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeningLinkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
